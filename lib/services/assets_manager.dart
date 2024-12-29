513a8c434920c1c7a4891f43eba78a3364abc9ba
Erhan Koyuncu <erhan@example.com>
2024-12-30 01:28:01 +0300

    Added assets and updated .gitignore file

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,104 @@
+class AssetsManager {
+  static String imagesPath = "assets/images";
+  static String profileImagePath = "$imagesPath/profile";
+  static String profileImagePath512 = "$profileImagePath/512";
+  static String categoriesPath = "$imagesPath/categories";
+  static String categoriesPath112 = "$categoriesPath/112";
+  static String bannerPath = "$imagesPath/banner";
+  static String bagPath = "$imagesPath/bag";
+
+  static String addMap = "$imagesPath/add_map.png";
+  static String error = "$imagesPath/error.png";
+  static String forget = "$imagesPath/forget.png";
+  static String roundenMap = "$imagesPath/rounden_map.png";
+  static String successful = "$imagesPath/successful.png";
+  static String search = "$imagesPath/search.png";
+  static String warning = "$imagesPath/warning.png";
+  static String settings = "$imagesPath/settings3.png";
+
+  static String clock = "$profileImagePath/clock.png";
+  static String location = "$profileImagePath/location.png";
+  static String login = "$profileImagePath/login.png";
+  static String logout = "$profileImagePath/logout.png";
+  static String privacy = "$profileImagePath/privacy.png";
+  static String theme = "$profileImagePath/theme.png";
+
+  static String clock512 = "$profileImagePath512/clock.png";
+  static String location512 = "$profileImagePath512/location.png";
+  static String login512 = "$profileImagePath512/login.png";
+  static String logout512 = "$profileImagePath512/logout.png";
+  static String privacy512 = "$profileImagePath512/privacy.png";
+  static String theme512 = "$profileImagePath512/theme.png";
+  static String user512 = "$profileImagePath512/user.png";
+
+  static String computer = "$categoriesPath/computer.png";
+  static String computer2 = "$categoriesPath/computer2.png";
+  static String computer3 = "$categoriesPath/computer3.png";
+  static String computer4 = "$categoriesPath/computer4.png";
+  static String file = "$categoriesPath/file.png";
+  static String file2 = "$categoriesPath/file2.png";
+  static String file3 = "$categoriesPath/file3.png";
+  static String flower = "$categoriesPath/flower.png";
+  static String flower2 = "$categoriesPath/flower2.png";
+  static String mobilePhone = "$categoriesPath/mobilePhone.png";
+  static String mobilePhone2 = "$categoriesPath/mobilePhone2.png";
+  static String phone = "$categoriesPath/phone.png";
+  static String shoes = "$categoriesPath/shoes.png";
+  static String shoes2 = "$categoriesPath/shoes2.png";
+  static String shoes3 = "$categoriesPath/shoes3.png";
+  static String shoes4 = "$categoriesPath/shoes4.png";
+  static String tshort = "$categoriesPath/tshort.png";
+  static String tshort2 = "$categoriesPath/tshort2.png";
+  static String tshort3 = "$categoriesPath/tshort3.png";
+  static String watch = "$categoriesPath/watch.png";
+  static String watch2 = "$categoriesPath/watch2.png";
+  static String watch3 = "$categoriesPath/watch3.png";
+  static String watch4 = "$categoriesPath/watch4.png";
+
+  static String computer112 = "$categoriesPath112/computer.png";
+  static String computer2112 = "$categoriesPath112/computer2.png";
+  static String computer3112 = "$categoriesPath112/computer3.png";
+  static String computer4112 = "$categoriesPath112/computer4.png";
+  static String file112 = "$categoriesPath112/file.png";
+  static String file2112 = "$categoriesPath112/file2.png";
+  static String file3112 = "$categoriesPath112/file3.png";
+  static String flower112 = "$categoriesPath112/flower.png";
+  static String flower2112 = "$categoriesPath112/flower2.png";
+  static String mobilePhone112 = "$categoriesPath112/mobilePhone.png";
+  static String mobilePhone2112 = "$categoriesPath112/mobilePhone2.png";
+  static String phone112 = "$categoriesPath112/phone.png";
+  static String shoes112 = "$categoriesPath112/shoes.png";
+  static String shoes2112 = "$categoriesPath112/shoes2.png";
+  static String shoes3112 = "$categoriesPath112/shoes3.png";
+  static String shoes4112 = "$categoriesPath112/shoes4.png";
+  static String tshort112 = "$categoriesPath112/tshort.png";
+  static String tshort2112 = "$categoriesPath112/tshort2.png";
+  static String tshort3112 = "$categoriesPath112/tshort3.png";
+  static String watch112 = "$categoriesPath112/watch.png";
+  static String watch2112 = "$categoriesPath112/watch2.png";
+  static String watch3112 = "$categoriesPath112/watch3.png";
+  static String watch4112 = "$categoriesPath112/watch4.png";
+
+  static String slide1 = "$bannerPath/1.png";
+  static String slide2 = "$bannerPath/2.png";
+  static String slide3 = "$bannerPath/3.png";
+  static String slide4 = "$bannerPath/4.png";
+  static String slide5 = "$bannerPath/5.png";
+  static String slide6 = "$bannerPath/6.png";
+  static String slide7 = "$bannerPath/7.png";
+
+  static String bagImg1 = "$bagPath/1.png";
+  static String bagImg2 = "$bagPath/2.png";
+  static String bagImg5 = "$bagPath/5.png";
+  static String bagImg6 = "$bagPath/6.png";
+  static String bagImg7 = "$bagPath/7.png";
+  static String bagImages1 = "$bagPath/bag.png";
+  static String bagImages2 = "$bagPath/bag2.png";
+  static String bagImages3 = "$bagPath/bag3.png";
+  static String bagImages4 = "$bagPath/bag4.png";
+  static String card = "$bagPath/card.png";
+  static String card2 = "$bagPath/card2.png";
+  static String card3 = "$bagPath/woman.png";
+
+
+}
